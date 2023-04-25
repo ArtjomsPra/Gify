931b971518472966e47be2eb36af5c1de58f46b8
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-04-25 15:41:59 +0300

    Add files via upload

---
+++ App/Gify.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+class Gify
+{
+    private string $name;
+    private string $url;
+    private string $id;
+    private string $rating;
+
+    public function __construct (string $name, string $url, string $id, string $rating) {
+        $this->name = $name;
+        $this->url = $url;
+        $this->id = $id;
+        $this->rating = $rating;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getRating(): string
+    {
+        return $this->rating;
+    }
+}
